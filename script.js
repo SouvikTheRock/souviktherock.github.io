417d7cbf310906a39e68dbd83bf8a36cb3bb9e83
SouvikTheRock <SouvikTheRock@users.noreply.github.com>
2021-01-05 23:19:50 +0530

    Minor Issues Fixed

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ window.onscroll=function(){
         }
     }
 }
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector( 
+          "body").style.visibility = "hidden"; 
+        document.querySelector( 
+          "#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector( 
+          "#loader").style.display = "none"; 
+        document.querySelector( 
+          "body").style.visibility = "visible"; 
+    } 
+}; 
